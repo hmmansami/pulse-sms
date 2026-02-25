7f992782363533335aa3b28075477a8bc083be28
Mohammed Samo <hmmansami@gmail.com>
2026-02-25 03:06:21 +0300

    Use experimental.serverComponentsExternalPackages for Next 14.1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ["bcryptjs", "@prisma/client", "@auth/core", "next-auth"],
+  experimental: {
+    serverComponentsExternalPackages: ["bcryptjs", "@prisma/client", "@auth/core", "next-auth"],
+  },
 };
 
 module.exports = nextConfig;
