5beba0251f8013b7cfa76805ff81b93e2687416f
Mohammed Samo <hmmansami@gmail.com>
2026-02-25 03:10:33 +0300

    Force webpack externals for @auth/core

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["bcryptjs", "@prisma/client", "@auth/core", "next-auth"],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "@auth/core": "commonjs @auth/core",
+      });
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
